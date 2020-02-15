9db173d397bfb84ab1e6ce35e666d3c52469fcaa
Yingqi Chen <chanwk18@hotmail.com>
2020-02-15 02:03:40 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+
+import React from 'react'
+import Component from 'react'
+
+class Keypad extends Component{
+
+    handleKeyUp = () =>(
+        console.log("Entering password...")
+    )
+
+    render(){
+        return(
+            <input type="password" onKeyUp={this.handleKeyUp}></input>
+        )
+    }
+
+}
+
+export default Keypad;
